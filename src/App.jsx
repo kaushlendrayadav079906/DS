0d66a928f8e6ba68ac165f16e5fabc7001d47060
kaushlendrayadav079906 <shubhamyadav079906@gmail.com>
2026-01-15 23:18:38 +0530

    Fix double Router error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import Application from './pages/Application.jsx'
 import Contact from './pages/Contact.jsx'
 import Home from './pages/Home.jsx'
@@ -8,16 +8,14 @@ import Splash from './pages/Splash.jsx'
 
 export default function App() {
   return (
-    <BrowserRouter >
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/application" element={<Application />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:slug" element={<ProjectDetail />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Splash />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/application" element={<Application />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/projects/:slug" element={<ProjectDetail />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
